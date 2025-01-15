10f9c18e2b01259839ff188ce57bcbfe80d9911e
tamerdarwish <74076970+tamerdarwish@users.noreply.github.com>
2025-01-15 14:30:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,33 +6,30 @@ const bodyParser = require('body-parser');
 // Load environment variables
 dotenv.config();
 const app = express();
-const allowedOrigins = ['http://26.206.131.69:3000','http://localhost:3000'];
 
 // Use CORS middleware
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true // إذا كنت بحاجة إلى إرسال ملفات تعريف الارتباط
 }));
-  
+
 app.use(express.json());
+app.use(bodyParser.json({ limit: '500mb' }));
+app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
 
-// إعداد body-parser مع تحديد الحد الأقصى
-app.use(bodyParser.json({ limit: '500mb' })); // تحديد الحد الأقصى لحجم JSON
-app.use(bodyParser.urlencoded({ limit: '500mb', extended: true })); // تحديد الحد الأقصى لـ URL-encoded
 // Import routes
-const eventsRouter = require('./routes/events');
-const photosRouter = require('./routes/photos');
-const usersRouter = require('./routes/users');
-const ordersRouter = require('./routes/orders');
-const uploadRoutes = require('./routes/upload');
-const productsRoutes = require('./routes/products');
-const contactRoutes = require('./routes/contactRoutes'); 
-const printRoutes = require('./routes/print'); 
-const adminRoutes = require('./routes/adminRoutes');
-const audiosRoutes = require('./routes/audios')
-const businessesRoutes = require('./routes/businesses')
-
+const eventsRouter = require('../routes/events');
+const photosRouter = require('../routes/photos');
+const usersRouter = require('../routes/users');
+const ordersRouter = require('../routes/orders');
+const uploadRoutes = require('../routes/upload');
+const productsRoutes = require('../routes/products');
+const contactRoutes = require('../routes/contactRoutes');
+const printRoutes = require('../routes/print');
+const adminRoutes = require('../routes/adminRoutes');
+const audiosRoutes = require('../routes/audios');
+const businessesRoutes = require('../routes/businesses');
 
 // Use routes
 app.use('/api/events', eventsRouter);
@@ -41,13 +38,13 @@ app.use('/api/users', usersRouter);
 app.use('/api/orders', ordersRouter);
 app.use('/api/upload', uploadRoutes);
 app.use('/api/products', productsRoutes);
-app.use('/api/contact', contactRoutes); 
-app.use('/api/print', printRoutes); 
+app.use('/api/contact', contactRoutes);
+app.use('/api/print', printRoutes);
 app.use('/admin', adminRoutes);
-app.use('/api/audios', audiosRoutes)
-app.use('/api/businesses', businessesRoutes)
+app.use('/api/audios', audiosRoutes);
+app.use('/api/businesses', businessesRoutes);
 
-
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// الدالة الرئيسية في Vercel يجب أن تصدر التطبيق
+module.exports = (req, res) => {
+  app(req, res); // تحويل الطلبات إلى Express
+};
